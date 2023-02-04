2ea1a8bc43c11dfc2ea46e097f64550a5896b70b
VenantTumukunde <ventum11@gmail.com>
2023-02-04 14:13:28 +0200

    signup initial code

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-import express from 'express';
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-const app = express();
-const port = 5000;
+const app = require('./app');
 
-app.get('/', (req, res) => {
-  res.send('Welcome to chat application');
-});
+dotenv.config({ path: './config.env' });
 
+// update the password in db connection string
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+// Connecting to the database
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// Start the server
 app.listen(port, () => {
   process.stdout.write(`Now listening on port ${port}`);
 });
---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+    length: [3, 'A user name must have more or equal then 3 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'A user must have an email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'A user must have a password'],
+    minlength: [3, 'A password must have more or equal then 3 characters'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
