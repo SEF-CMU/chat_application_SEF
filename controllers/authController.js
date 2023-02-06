22fa20d358878e57427f20800f03f7dd0092b740
eliekayitare <kayitarelie@gmail.com>
2023-02-06 14:50:29 +0200

    login backend endpoint

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,13 @@
 import jwt from "jsonwebtoken";
 import User from "../models/userModel";
 import catchAsync from "../utils/catchAsync";
+import bcrypt from "bcryptjs";
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
+    phone: req.body.phone,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
   });
@@ -18,7 +20,7 @@ exports.signup = catchAsync(async (req, res, next) => {
   res.status(201).json({
     status: "success",
     message: "user created successfully",
-    // token: token,
+    token,
     data: {
       user: newUser,
     },
@@ -26,28 +28,43 @@ exports.signup = catchAsync(async (req, res, next) => {
 });
 
 exports.login = catchAsync(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
 
-  const user = User.findOne({ email: req.body.email });
+  if (!user) {
+    // return next(new AppError("Email doesn't exist", 401));
+    return res.status(401).send("Email doesn't exist");
+  }
 
-  if (!user) { return next(new AppError("Email doesn't exist", 401)); }
+  const isValid = await bcrypt.compare(req.body.password, user.password);
 
-  const isPasswordCorrect = await user.correctPassword(req.body.password, user.password);
+  if (!isValid) {
+    return res.status(401).send("Password is incorrect");
+  }
 
-  if (!isPasswordCorrect) { return next(new AppError("Incorrect password", 401)); }
+  const token = jwt.sign(
+    { id: user.id, name: user.name, email: user.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    }
+  );
 
-  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {  
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  res.header("auth-token", token).send(token);
 
-  res.status(200).json({
-    status: "success",
-    message: "user logged in successfully",
-    // token: token,
-    data: {
-      user: user,
-    },
-    
-  });
-
-  
+  // res.status(200).json({
+  //   status: "success",
+  //   message: "user logged in successfully",
+  //   token,
+  //   data: {
+  //     user: user,
+  //   },
+  // });
 });
+
+exports.logout = (req, res) => {
+  res.cookie("jwt", "loggedout", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({ status: "Logged out successfully" });
+};
